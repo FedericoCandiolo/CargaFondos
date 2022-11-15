8067c077fc9f918075e7faf1b186b1c761486fef
joaquin <jsancheznicolai@gmail.com>
2022-11-15 18:09:10 -0300

    Error en app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var bodyParser = require("body-parser");user
+var bodyParser = require("body-parser");
 
 var User = require("./models/user").User;
 var Movement = require('./models/movement').Movement;
